8a8b250bd96af5c5475dab71eabe2d4e50a64f43
Julia Stachow <jstachow@agh.student.edu.pl>
2022-01-22 23:05:42 +0100

    utworzenie raportu

--- projektprojektowanieop/Raport.cpp
+++ projektprojektowanieop/Raport.cpp
@@ -1,8 +1,18 @@
 #include <exception>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 #include"Raport.h"
 
 void Raport::utworz_raport() {
-	throw "Not yet implemented";
+	fstream plik;
+	string tresc_raportu;
+
+	cout << "Podaj tresc raportu: " << endl;
+	cin >> tresc_raportu;
+
+	plik.open("raport.txt", ios::out | ios::app);
+	plik << tresc_raportu << "\n";
+	plik.close();
 }
