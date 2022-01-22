69c7c37568747a660cf86a64488b7593551602b4
Oliwia Stebelska <OliwkaS44@gmail.com>
2022-01-22 20:18:33 +0100

    dodanie funkcji menu_admina

--- projektprojektowanieop/Źródło.cpp
+++ projektprojektowanieop/Źródło.cpp
@@ -27,6 +27,68 @@ class Logowanie;
 class Admin;
 class Rejestracja;
 
+void menu_admina()
+{
+	System s;
+	Ksiazka ks;
+	Wypozyczenie w;
+	Zwrot z;
+	Baza_ksiazek b;
+
+	char x;
+
+	cout << "\n\n Zalogowanie na konto pracownika powiodlo sie!! Nacisnij jakikolwiek przycisk, aby kontynuowac..";
+	getchar();
+	getchar();
+	system("cls");
+
+	int choice = 0;
+	while (choice != 9)
+	{
+
+		cout << "\n1. Dodaj ksiazke";
+		cout << "\n2. Usun ksiazke";
+		cout << "\n3. Wyswietl wszystkie ksiazki";
+		cout << "\n4. Wypozycz";
+		cout << "\n5. Zwroc";
+		cout << "\n9. Exit";
+		cout << "\n\n Wpisz swoj wybor : ";
+
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			do {
+				b.dodaj_ksiazke();
+				cout << "\n\nCzy chcesz dodac kolejna ksiazke?" << "(y/n) : ";
+				cin >> x;
+			} while (x == 'y');
+			break;
+
+		case 2: b.usun_ksiazke(); break;
+		case 3: b.szukaj(); break;
+		case 4:
+			cout << "Zeskanuj ksiazke." << endl;
+			w.zeskanuj_ksiazke();
+			break;
+		case 5:
+			cout << "Zeskanuj ksiazke." << endl;
+			if (ks.kara = true)
+				z.oplata();
+			else
+				cout << "ZWROT PRZEBIEGL POMYSLNIE." << endl;
+			break;
+		case 9: exit(0); break;
+
+		default:
+		{
+			cout << "\n\nZly wybor.  Nacisnij jakikolwiek przycisk, aby kontynuowac..";
+			getchar();
+		}
+		}
+		system("cls");
+	}
+}
 void panel_sterowania()
 {
 	string adminUser;
@@ -119,52 +181,7 @@ void panel_sterowania()
 
 		if (adminUser == "root" && adminPassword == "root")
 		{
-			cout << "\n\n Zalogowanie na konto pracownika powiodlo sie!! Nacisnij jakikolwiek przycisk, aby kontynuowac..";
-			getchar();
-			getchar();
-			system("cls");
-
-			int choice = 0;
-			while (choice != 9)
-			{
-
-				cout << "\n1. Dodaj ksiazke";
-				cout << "\n2. Usun ksiazke";
-				cout << "\n3. Wyswietl wszystkie ksiazki";
-				cout << "\n4. Wypozycz";
-				cout << "\n5. Zwroc";
-				cout << "\n9. Exit";
-				cout << "\n\n Wpisz swoj wybor : ";
-
-				cin >> choice;
-				switch (choice)
-				{
-				case 1:
-					do {
-						b.dodaj_ksiazke();
-						cout << "\n\nCzy chcesz dodac kolejna ksiazke?" << "(y/n) : ";
-						cin >> x;
-					} while (x == 'y');
-					break;
-
-				case 2: b.usun_ksiazke(); break;
-				case 3: b.szukaj(); break;
-				case 4: w.zeskanuj_ksiazke(); break;
-				case 5:
-					cout << "Zeskanuj ksiazke.";
-
-					if (ks.kara = true)
-						z.oplata();
-					else
-						cout << "ZWROT PRZEBIEGL POMYSLNIE.";
-					break;
-				case 9: exit(0); break;
-
-				default: cout << "\n\nZly wybor.  Nacisnij jakikolwiek przycisk, aby kontynuowac..";
-					getchar();
-				}
-				system("cls");
-			}
+			menu_admina();
 		}
 		else
 		{
