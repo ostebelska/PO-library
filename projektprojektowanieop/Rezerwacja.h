4c7c4c70b801098cb4a52960b6a6a20e9a88f68f
JuliaStachow <77447992+JuliaStachow@users.noreply.github.com>
2022-01-22 18:01:11 +0100

    Update Rezerwacja.h

--- projektprojektowanieop/Rezerwacja.h
+++ projektprojektowanieop/Rezerwacja.h
@@ -5,22 +5,12 @@ using namespace std;
 class Uzytkownik;
 class Ksiazka;
 class Bibliotekarz;
-class Wypo¿yczenie;
+class Wypozyczenie;
 class Rezerwacja;
 
 class Rezerwacja
 {
-private: string _ksi¹¿ka;
-private: string _data_waznosci;
-private: int _numer_rezerwacji;
-public: Uzytkownik* _unnamed_U¿ytkownik_;
-public: Ksiazka* _unnamed_Ksi¹¿ka_;
-public: Bibliotekarz* _unnamed_Bibliotekarz_;
-public: Wypo¿yczenie* _unnamed_Wypo¿yczenie_;
-
 public: void sprawdz_rezerwacje();
 
 public: void zarezerwuj();
-
-public: void operation();
 };
